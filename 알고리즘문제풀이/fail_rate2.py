759bf6e7a5c7f0de061b18c96fbc6caeacea8b17
HyeminNoh <shrmffkfk@naver.com>
2019-12-23 18:03:41 +0900

    실패율 (통과 답안)

---
+++ 알고리즘문제풀이/fail_rate2.py
@@ -0,0 +1,10 @@
+def solution(N, stages):
+    result = {}
+    tatal_len = len(stages)
+    for stage in range(1, N+1):
+        if tatal_len != 0:
+            result[stage] = stages.count(stage) / tatal_len
+            tatal_len -= stages.count(stage)
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
